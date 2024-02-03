353951574fc7155c437b29dea0e0ddc6a4333850
Connadrek <151085771+Connadrek@users.noreply.github.com>
2024-02-03 14:36:49 +0100

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
+  ['8112480981', 'connadrek ', true],
   ['6281255369012', 'Ash', true], 
   [''] 
 ] //Number of owners
